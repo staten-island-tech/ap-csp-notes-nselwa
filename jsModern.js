2eb015b5d88a1da9c46e0d6285c12902bb4b2972
nselwa <50632491+nselwa@users.noreply.github.com>
2020-09-21 05:57:01 -0400

    Add files via upload

---
+++ jsModern.js
@@ -0,0 +1,5 @@
+let life = 100;
+
+life = life - 40;
+
+console.log(life);
---
+++ jsOld.js
@@ -0,0 +1,13 @@
+var life = 100; // This is a basic life bar. (number)
+
+life = life - 10; //100-10=90
+
+life = life / 2; //90/2=45
+
+var name = "Nathan"; // This is the name that's displayed. (simple text)
+
+var checkout = false; //Boolean
+
+var box = null;
+
+console.log(life);
